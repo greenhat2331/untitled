d15675e6145676b1fe72af3473e4e0561180ac8c
Yie Jason <2426880553@qq.com>
2023-06-26 16:58:26 +0800

    测试3:添加了试题2

---
+++ src/Case02/Arr01.java
@@ -0,0 +1,11 @@
+package Case02;
+
+public class Arr01 {
+    public static void main(String[] args) {
+        int [] arr = {1,22,32,76,57};
+        System.out.println("数组长为"+arr.length);//求数组长度
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
