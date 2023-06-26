b3ee6cf2ffd4de238914a8c4bb6a804a0de94076
Yie Jason <2426880553@qq.com>
2023-06-26 16:20:51 +0800

    测试1

---
+++ src/Sum.java
@@ -0,0 +1,10 @@
+//求连续数和
+public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i += 1) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
